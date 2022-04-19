12eec79a5a98eaf72529f226ece51afad583f9a2
dmitryjob4j <haoos@inbox.ru>
2022-04-19 07:56:11 +0300

    фильтрафия фильмов

--- src/main/java/ru/job4j/cinema/controller/SessionController.java
+++ src/main/java/ru/job4j/cinema/controller/SessionController.java
@@ -3,7 +3,16 @@ package ru.job4j.cinema.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.cinema.model.Session;
+import ru.job4j.cinema.model.Ticket;
 import ru.job4j.cinema.service.SessionService;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * 3. Мидл
@@ -27,6 +36,18 @@ public class SessionController {
     public String index(Model model) {
         model.addAttribute("sessions", serviceService.findAll());
         return "index";
+    }
+
+    @GetMapping("/selectTicket/{sessionId}")
+    public String formSelectTicket(Model model,
+                                   @PathVariable("sessionId") int id) {
+        model.addAttribute("cinema", serviceService.findById(id).get());
+        return "selectTicket";
+    }
+
+    @PostMapping("/selectTicket")
+    public String selectTicket(@ModelAttribute Ticket ticket) {
+        return "redirect:index";
     }
 }
 
--- src/main/java/ru/job4j/cinema/service/SessionService.java
+++ src/main/java/ru/job4j/cinema/service/SessionService.java
@@ -5,7 +5,10 @@ import ru.job4j.cinema.model.Session;
 import ru.job4j.cinema.persistence.SessionDBStore;
 import ru.job4j.cinema.persistence.Store;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * 3. Мидл
@@ -20,12 +23,54 @@ import java.util.List;
 @Service
 public class SessionService {
     private final Store<Session> store;
+    private final Map<Session,
+            ConcurrentHashMap<Integer, ConcurrentLinkedQueue<Integer>>> selectTicket = new ConcurrentHashMap<>();
 
     public SessionService(SessionDBStore store) {
         this.store = store;
+        initTicket();
+    }
+
+    private void initTicket() {
+        int row = 5;
+        int cell = 5;
+        List<Session> sessions = store.findAll();
+        for (Session session : sessions) {
+            this.selectTicket.computeIfAbsent(session, s -> {
+                ConcurrentHashMap<Integer, ConcurrentLinkedQueue<Integer>> rows = new ConcurrentHashMap<>();
+                for (int i = 1; i <= row; i++) {
+                    rows.computeIfAbsent(i, k -> {
+                        ConcurrentLinkedQueue<Integer> cells = new ConcurrentLinkedQueue<>();
+                        for (int j = 1; j <= cell; j++) {
+                            cells.add(j);
+                        }
+                        return cells;
+                    });
+                }
+                return rows;
+            });
+        }
+    }
+
+    public ConcurrentHashMap<Integer, ConcurrentLinkedQueue<Integer>> findAllTicket(Session session) {
+        return this.selectTicket.get(session);
+    }
+
+    public Optional<Session> findById(int id) {
+        return store.findById(id);
     }
 
     public List<Session> findAll() {
         return store.findAll();
+    }
+
+    public void test() {
+        Session session = new Session(0, "ddd");
+        ConcurrentHashMap<Integer, ConcurrentLinkedQueue<Integer>> l = findAllTicket(session);
+        for (ConcurrentLinkedQueue<Integer> element : l.values()) {
+            for (Integer i : element) {
+                System.out.println(i);
+            }
+        }
     }
 }
